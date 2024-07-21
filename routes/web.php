2477bcbea1950568bdd4184b4fd7bff344ad92dc
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-07-21 11:55:15 +0300

    routes resource

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WordController;
+use App\Http\Controllers\PhraseController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\LanguageController;
+use App\Http\Controllers\TranslationController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +19,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::resource('languages', LanguageController::class);
+    Route::resource('words', WordController::class);
+    Route::resource('phrases', PhraseController::class);
+    Route::resource('translations', TranslationController::class);
 });
 
 require __DIR__.'/auth.php';
