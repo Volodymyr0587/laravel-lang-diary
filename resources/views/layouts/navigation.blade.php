415c99bd0591f3abf340c9d9f53bc3fa2deff6db
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-07-22 12:43:04 +0300

    Add Languages menu item

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -17,6 +17,11 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('languages.index')" :active="request()->routeIs('languages.index')">
+                        {{ __('Languages') }}
+                    </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('words.index')" :active="request()->routeIs('words.index')">
                         {{ __('Words') }}
                     </x-nav-link>
@@ -77,10 +82,16 @@
             </x-responsive-nav-link>
         </div>
         <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('languages.index')" :active="request()->routeIs('languages.index')">
+                {{ __('Languages') }}
+            </x-responsive-nav-link>
+        </div>
+        <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('words.index')" :active="request()->routeIs('words.index')">
                 {{ __('Words') }}
             </x-responsive-nav-link>
         </div>
+
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
