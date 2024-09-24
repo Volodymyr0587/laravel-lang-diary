a2e018f055e423650a807114dd200b567d2fe592
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-09-24 16:06:01 +0300

    Add links to register and login form to sign in with google

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,6 +33,8 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
+
+            <a href="{{ route('google.redirect') }}" class="mr-6"> Login with Google </a>
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
